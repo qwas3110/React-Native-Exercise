434c7dcc63a2f86a48d46aaa90993a868216a7e3
xhw <283994917@qq.com>
2019-11-05 13:30:26 +0800

    AddEntry 中更新图标

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -2,11 +2,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from "./UdaciSlider";
 import UdaciStepper from "./UdaciStepper";
 import DateHeader from "./DateHeader";
-
+import TextButton from "./TextButton";
 
 const SubmitBtn = ({ onPress }) => {
     return (
@@ -28,6 +29,17 @@ export default class AddEntry extends Component {
         swim: 0,
         sleep: 0,
         eat: 0,
+    };
+
+
+    reset = () => {
+        const key = timeToString();
+
+        // Update Redux
+
+        // Route to Home
+
+        // Update DB
     };
 
     submit = () => {
@@ -77,6 +89,16 @@ export default class AddEntry extends Component {
 
     render() {
         const metaInfo = getMetricMetaInfo();
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons name="ios-happy" size={100} />
+                    <Text>You already logged your information for today.</Text>
+                    <TextButton onPress={this.reset}>Reset</TextButton>
+                </View>
+            );
+        }
 
         return (
             <View>
