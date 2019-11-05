0043944baddebe725554bfaf2c2b10c5b8cafb37
xhw <283994917@qq.com>
2019-11-05 21:56:33 +0800

    UdaciStepper 样式

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
   render() {
     return (
        <Provider store={createStore(reducer)}>
-         <View style={styles.container}>
+         <View style={{flex: 1}}>
            <AddEntry />
          </View>
        </Provider>
@@ -23,11 +23,11 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// });
