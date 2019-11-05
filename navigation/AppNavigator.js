ebace2bee42edcc4abccb06b08b2d378d72263ac
xhw <283994917@qq.com>
2019-11-05 22:22:19 +0800

    AppNavigator 组件

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,6 @@
+// AppNavigator.js
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import TabNavigator from './TabNavigator';
+
+export default createAppContainer(TabNavigator);
