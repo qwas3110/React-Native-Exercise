9c6a2c66a1c0ab9d9701beac3f8f5d618a4ad4c7
xhw <283994917@qq.com>
2019-11-05 21:51:42 +0800

    应用样式布局

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,7 +1,8 @@
 // AddEntry.js
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native';
+import { white, purple } from '../utils/colors';
 import { Ionicons } from '@expo/vector-icons';
 import { getMetricMetaInfo, timeToString,getDailyReminderValue } from '../utils/helpers';
 import {submitEntry,removeEntry} from "../utils/api";
@@ -20,11 +21,18 @@ import TextButton from "./TextButton";
 
 const SubmitBtn = ({ onPress }) => {
     return (
-        <TouchableOpacity onPress={onPress}>
-            <Text>SUBMIT</Text>
+        <TouchableOpacity
+            style={
+                Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn
+            }
+            onPress={onPress}
+        >
+            <Text style={styles.submitBtnText}>SUBMIT</Text>
         </TouchableOpacity>
     );
 };
+
+
 SubmitBtn.propTypes = {
     onPress: PropTypes.func.isRequired
 };
@@ -82,32 +90,6 @@ class AddEntry extends Component {
     };
 
 
-    reset = () => {
-        const key = timeToString();
-
-        // Update Redux
-
-        // Route to Home
-
-        // Update DB
-    };
-
-    submit = () => {
-        const key = timeToString()
-        const entry = this.state
-
-        // Update Redux
-
-        this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
-
-        // Navigate to home
-
-        // Save to "DB"
-
-        // Clear local notification
-    };
-
-
     increment = (metric) => {
         const { max, step } = getMetricMetaInfo(metric)
 
@@ -142,23 +124,28 @@ class AddEntry extends Component {
 
         if (this.props.alreadyLogged) {
             return (
-                <View>
-                    <Ionicons name="ios-happy" size={100} />
+                <View style={styles.center}>
+                    <Ionicons
+                        name={Platform.OS === 'ios' ? 'ios-happy' : 'md-happy'}
+                        size={100}
+                    />
                     <Text>You already logged your information for today.</Text>
-                    <TextButton onPress={this.reset}>Reset</TextButton>
+                    <TextButton style={{ padding: 10 }} onPress={this.reset}>
+                        Reset
+                    </TextButton>
                 </View>
             );
         }
 
         return (
-            <View>
-                <DateHeader date={(new Date()).toLocaleDateString()}/>
+            <View style={styles.container}>
+                <DateHeader date={new Date().toLocaleDateString()} />
                 {Object.keys(metaInfo).map(key => {
                     const { getIcon, type, ...rest } = metaInfo[key];
                     const value = this.state[key];
 
                     return (
-                        <View key={key}>
+                        <View key={key} style={styles.row}>
                             {getIcon()}
                             {type === 'slider' ? (
                                 <UdaciSlider
@@ -182,6 +169,51 @@ class AddEntry extends Component {
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: white
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    iosSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40
+    },
+    androidSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30
+    }
+});
 
 
 function mapStateToProps(state) {
