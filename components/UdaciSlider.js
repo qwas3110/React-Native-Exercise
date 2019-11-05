6e458f08579d5529282dd144944432113bbf9656
xhw <283994917@qq.com>
2019-11-05 13:15:12 +0800

    changes for AddEntry-methods

---
+++ components/UdaciSlider.js
@@ -0,0 +1,13 @@
+// UdaciSlider.js
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class UdaciSlider extends Component {
+    render() {
+        return (
+            <View>
+                <Text> UdaciSlider </Text>
+            </View>
+        );
+    }
+}
---
+++ components/UdaciStepper.js
@@ -0,0 +1,13 @@
+// UdaciStepper.js
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class UdaciStepper extends Component {
+    render() {
+        return (
+            <View>
+                <Text> UdaciStepper </Text>
+            </View>
+        );
+    }
+}
