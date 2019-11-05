3733ccfbee594335c8fcda2517ba62fca1f2d309
xhw <283994917@qq.com>
2019-11-05 13:22:52 +0800

    步进器组件

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,13 +1,40 @@
 // UdaciStepper.js
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 
-export default class UdaciStepper extends Component {
-    render() {
-        return (
+export default function UdaciStepper({
+                                         max,
+                                         unit,
+                                         step,
+                                         value,
+                                         onIncrement,
+                                         onDecrement
+                                     }) {
+    return (
+        <View>
             <View>
-                <Text> UdaciStepper </Text>
+                <TouchableOpacity onPress={onDecrement}>
+                    <FontAwesome name="minus" size={30} color={'black'} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onIncrement}>
+                    <FontAwesome name="plus" size={30} color={'black'} />
+                </TouchableOpacity>
             </View>
-        );
-    }
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
+        </View>
+    );
 }
+
+UdaciStepper.propTypes = {
+    max: PropTypes.number.isRequired,
+    unit: PropTypes.string.isRequired,
+    step: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+};
