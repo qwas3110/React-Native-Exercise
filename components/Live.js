91b814ffba22fc0a52e5c92113304a226f0e6738
xhw <283994917@qq.com>
2019-11-07 17:47:17 +0800

    Live 拒绝状态

--- components/Live.js
+++ components/Live.js
@@ -15,7 +15,7 @@ import { purple, white } from '../utils/colors';
 export default class Live extends Component {
     state = {
         coords: null,
-        status: 'undetermined',
+        status: 'denied',
         direction: ''
     };
 
@@ -47,8 +47,15 @@ export default class Live extends Component {
 
         if (status === 'denied') {
             return (
-                <View>
-                    <Text>Denied</Text>
+                <View style={styles.center}>
+                    <Foundation name="alert" size={50} />
+                    <Text>
+                        You denied your location. You can fix this by visiting your settings
+                        and enabling location services for this app.
+                    </Text>
+                    <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+                        <Text style={styles.buttonText}>Enable</Text>
+                    </TouchableOpacity>
                 </View>
             );
         }
