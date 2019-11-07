90811ef8cfdc7d9ad9b5e4ac08afc9f5f8b2ea7e
xhw <283994917@qq.com>
2019-11-07 17:45:51 +0800

    Live 不确定状态

--- components/Live.js
+++ components/Live.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator } from 'react-native';
+import {
+    Text,
+    View,
+    ActivityIndicator,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+import {Foundation} from "@expo/vector-icons";
+import { purple, white } from '../utils/colors';
+
+
+
 
 export default class Live extends Component {
     state = {
         coords: null,
-        status: null,
+        status: 'undetermined',
         direction: ''
     };
+
+    askPermission = () => {
+        alert('clicked');
+    };
+
+
     render() {
-        const { coords, direction } = this.state;
+        const { status, coords, direction } = this.state;
 
-        const status = 'undetermined';
 
-        // debugger;
+
+        if (status === 'undetermined') {
+            return (
+                <View style={styles.center}>
+                    <Foundation name="alert" size={50} />
+                    <Text>You need to enable location services for this app.</Text>
+                    <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+                        <Text style={styles.buttonText}>Enable</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
 
         if (status === null) {
             return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -26,13 +53,7 @@ export default class Live extends Component {
             );
         }
 
-        if (status === 'undetermined') {
-            return (
-                <View>
-                    <Text>undetermined</Text>
-                </View>
-            );
-        }
+
 
         return (
             <View>
@@ -42,3 +63,29 @@ export default class Live extends Component {
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30
+    },
+    button: {
+        padding: 10,
+        backgroundColor: purple,
+        alignSelf: 'center',
+        borderRadius: 5,
+        margin: 20
+    },
+    buttonText: {
+        color: white,
+        fontSize: 20
+    }
+});
